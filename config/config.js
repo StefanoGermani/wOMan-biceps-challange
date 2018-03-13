8d337686e4759211921c19641bdfde502da0f093
Stefano Germani <stefger88@gmail.com>
2018-03-14 00:30:15 +0100

    removed unused config

--- config/config.js
+++ config/config.js
@@ -5,27 +5,18 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
   development: {
     root: rootPath,
-    app: {
-      name: 'delta'
-    },
     port: process.env.PORT || 3000,
     db: 'mongodb://localhost/delta-development'
   },
 
   test: {
     root: rootPath,
-    app: {
-      name: 'delta'
-    },
     port: process.env.PORT || 3000,
     db: 'mongodb://localhost/delta-test'
   },
 
   production: {
     root: rootPath,
-    app: {
-      name: 'delta'
-    },
     port: process.env.PORT || 3000,
     db: 'mongodb://localhost/delta-production'
   }
