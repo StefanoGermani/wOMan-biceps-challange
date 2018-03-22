4628e7ea88477b803049caf5c332acf5bbfdcc75
Stefano Germani <stefger88@gmail.com>
2018-03-22 15:09:43 +0100

    fixed update concorrente

--- app/controllers/gara.js
+++ app/controllers/gara.js
@@ -12,10 +12,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  var concorrente = Concorrente.find({ codice: req.body.codice }).exec();
-  concorrente.ripetute = req.body.ripetute;
-  concorrente.peso = req.body.peso;
-  concorrente.update(function(err) {
+  Concorrente.update({ codice: req.body.codice }, { $set: { ripetute: req.body.ripetute, peso: req.body.peso }}, (err) => {
     if (err) return next(err);
     res.render('gara');
   });
