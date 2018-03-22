cc5a8df6a94fd08ba8f02838fada3026e43e2807
Stefano Germani <stefger88@gmail.com>
2018-03-22 22:28:04 +0100

    added round rankings

--- app/controllers/classifiche.js
+++ app/controllers/classifiche.js
@@ -16,3 +16,11 @@ router.get('/', (req, res, next) => {
   });
 });
 
+router.get('/turno/:turno', (req, res, next) => {
+  Concorrente.find({ turno: req.params.turno }).limit(20).sort({ totale: 'desc' }).exec((err, concorrenti) => {
+    if (err) return next(err);
+    res.render('index', {
+      concorrenti: concorrenti
+    });
+  });
+});
